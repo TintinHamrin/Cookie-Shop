15d68317a4bf9285fccf471bc3452aea52060205
Tintin Hamrin <tintin.hamrin@hyperisland.se>
2022-03-13 18:37:35 +0100

    added proxy and fetching data to frontend

--- backend/index.ts
+++ backend/index.ts
@@ -1,28 +1,25 @@
 const express = require('express');
 const app = express();
+const { MongoClient } = require('mongodb');
+var url =
+  'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.2.2';
 
-app.get('/get', (req: any, res: any) => {
-  console.log('get me baby!!');
-  res.json([
-    {
-      id: 1,
-      url: '../assets/gaby-dyson-QX814A1w3j4-unsplash.jpg',
-    },
-    {
-      id: 2,
-      url: '../assets/james-coleman-5HR1gItc7Gs-unsplash.jpg',
-    },
-    {
-      id: 3,
-      url: '../assets/leo-roza-y6BYMYP3vD4-unsplash.jpg',
-    },
-    {
-      id: 4,
-      url: '../assets/yosep-sugiarto-DE-ZospqOqU-unsplash.jpg',
-    },
-  ]);
+var client: any;
+
+MongoClient.connect(url, function (err: any, c: any) {
+  client = c;
+  if (err) throw err;
+  console.log('Database created!');
+});
+
+app.get('/products', async (req: any, res: any) => {
+  const db = client.db('cookieShop');
+  var cursor = db.collection('products').find({});
+
+  const allValues = await cursor.toArray();
+  res.json(allValues);
 });
 
 app.listen(3001, () => {
-  console.log('listening on 3001!!!');
+  console.log('listening on 3001!');
 });
--- src/Products.tsx
+++ src/Products.tsx
@@ -3,35 +3,34 @@ import ProductCard from './ProductCard';
 import './Products.scss';
 
 type Product = {
+  //make a class for product?
   id: number;
   url: string;
 };
 
 function Products() {
-  // const products: any[] = [];
   const [products, setProducts] = useState<Array<Product>>([]);
 
   useEffect(() => {
-    fetch('/get', {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => setProducts(data))
-      .catch((e) => console.log(e));
-    console.log(products);
+    (async () => {
+      const data = await fetch('/products');
+      const json = await data.json();
+      setProducts(json);
+      console.log(json);
+      console.log(json.length);
+    })();
   }, []);
 
   return (
     <div className="products">
       <h1>Products</h1>
-      {products.map((product) => (
+      <p>{products.length}</p>
+      {/* {products.map((product) => (
         // <p key={Math.random()}>
         //   itemId: {product.id}, itemUrl: {product.url}
         // </p
         <ProductCard />
-      ))}
+      ))} */}
     </div>
   );
 }
