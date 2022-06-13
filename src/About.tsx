cabf5aab44fddae018df67690453e35ac6b8d16f
Tintin Hamrin <tintinhamrin@gmail.com>
2022-06-13 15:50:42 +0200

    test commit for firing github actions

--- src/About.tsx
+++ src/About.tsx
@@ -72,7 +72,7 @@ export default function About() {
           }}
         />
         <Button sx={{ color: "secondary.main" }} onClick={sendDataToDb}>
-          Click to add
+          Click to add!
         </Button>
         {feedbackText}
       </Box>
